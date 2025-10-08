77e40b3276dcef4fc69c75a29e7a1bf0a4ebb77c
fogvn <dn135897@gmail.com>
2025-10-08 17:23:01 +0700

    sercure variable of firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,17 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC-hwYDz4n5w80f5V-iUt5sAfmjclDkuuA",
-    authDomain: "portfolio-9cd55.firebaseapp.com",
-    databaseURL: "https://portfolio-9cd55-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "portfolio-9cd55",
-    storageBucket: "portfolio-9cd55.firebasestorage.app",
-    messagingSenderId: "265388060433",
-    appId: "1:265388060433:web:ba34930519ea0371f326b0",
-    measurementId: "G-8ZS3ZLVSDX"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
